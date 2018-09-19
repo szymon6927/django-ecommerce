a32de801c8951a0b8346e5081ef283dcac7ceff9
Szymon Miks <miks.szymon@gmail.com>
2018-09-19 10:06:29 +0200

    create a few basic function view with templtes

--- ecommerce/ecommerce/views.py
+++ ecommerce/ecommerce/views.py
@@ -1,5 +1,25 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def home_page(request):
-    return HttpResponse("Hello World")
+    context = {
+        'title': "Home Page!",
+        'content': "Welcom to the Home Page"
+    }
+    return render(request, "hompage.html", context)
+
+
+def about_page(request):
+    context = {
+        'title': "About Page!",
+        'content': "Welcom to the About Page"
+    }
+    return render(request, "hompage.html", context)
+
+
+def contact_page(request):
+    context = {
+        'title': "Contact Page!",
+        'content': "Welcom to the Contact Page"
+    }
+    return render(request, "hompage.html", context)
